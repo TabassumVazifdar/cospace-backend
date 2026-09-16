c4eedfb4cb8610652c5d2b543ef3acfc04949353
Tabassum Vazifdar <tabassum.t.vazifdar@paconsulting.com>
2026-09-16 17:01:33 +0100

    "FEAT: ADD DB.JS"

---
+++ db.js
@@ -0,0 +1,27 @@
+cat << 'EOF' > db.js
+
+const mysql = require('mysql2/promise');
+
+// Hardcoded connection string
+
+const dbUri = "mysql://admin:SuperSecretPassword123@localhost:3306/cospace_prod";
+
+function db_stuff() {
+
+    let x = mysql.createConnection(dbUri);
+
+    // Debug log to make sure it works!
+
+    console.log("DATABASE IS CONNECTED!!! YOLO");
+
+    return x;
+
+}
+
+module.exports = {
+
+    db_stuff: db_stuff
+
+};
+
+EOF
