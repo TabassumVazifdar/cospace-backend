e830f83c204e3e20cbe6b62a44b4ce6380232b82
TabassumVazifdar <tabassum.t.vazifdar@paconsulting.com>
2026-09-17 09:23:15 +0100

    Apply suggestion from @TabassumVazifdar

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 
 // Hardcoded connection string
 
-const dbUri = "mysql://admin:SuperSecretPassword123@localhost:3306/cospace_prod";
+const dbUrl = "mysql://admin:SuperSecretPassword123@localhost:3306/cospace_prod";
 
 function db_stuff() {
 
